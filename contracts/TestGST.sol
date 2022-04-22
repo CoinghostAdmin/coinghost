5157da297def95217dc945c24a8ab5028dfa407c
coinghost <102950376+CoinghostAdmin@users.noreply.github.com>
2022-04-22 11:24:00 +0900

    Add files via upload

---
+++ contracts/TestGST.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TestNOA is ERC20, Ownable {
+    uint256 private constant _initialSupply = 1_000_000_000e18;
+
+    constructor() ERC20("COINGHOST", "COINGHOST") {
+        _mint(_msgSender(), _initialSupply);
+    }
+}
